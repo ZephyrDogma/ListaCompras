ac1a777f97dc5315e5540f2f6540f51b61bf1f42
ZephyrDogma <77338971+ZephyrDogma@users.noreply.github.com>
2021-05-06 00:04:25 +0100

    Importei o ConsoleApp e o wsOperations

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            localhost.WebService1 ws = new localhost.WebService1();
+            Console.WriteLine("versão EN" + ws.HelloWorld());
+        }
+    }
+}
